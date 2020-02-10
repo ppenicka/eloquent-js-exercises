1f2f72a69c81557981bcf41d6fc788ff89b12a70
Petr Penicka <petr.penicka@gmail.com>
2020-02-10 15:21:42 +0100

    Solutions to Chapter 7 except Robot Efficiency exercise

---
+++ chapter7.js
@@ -0,0 +1,199 @@
+const roads = [
+  "Alice's House-Bob's House",   "Alice's House-Cabin",
+  "Alice's House-Post Office",   "Bob's House-Town Hall",
+  "Daria's House-Ernie's House", "Daria's House-Town Hall",
+  "Ernie's House-Grete's House", "Grete's House-Farm",
+  "Grete's House-Shop",          "Marketplace-Farm",
+  "Marketplace-Post Office",     "Marketplace-Shop",
+  "Marketplace-Town Hall",       "Shop-Town Hall"
+];
+
+function buildGraph(edges) {
+  let graph = Object.create(null);
+  function addEdge(from, to) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
+    }
+  }
+  for (let [from, to] of edges.map(r => r.split("-"))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+  return graph;
+}
+
+const roadGraph = buildGraph(roads);
+
+class VillageState {
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    } else {
+      let parcels = this.parcels.map(p => {
+        if (p.place != this.place) return p;
+        return {place: destination, address: p.address};
+      }).filter(p => p.place != p.address);
+      return new VillageState(destination, parcels);
+    }
+  }
+}
+
+function runRobot(state, robot, memory) {
+  for (let turn = 0;; turn++) {
+    if (state.parcels.length == 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
+
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+
+function randomRobot(state) {
+  return {direction: randomPick(roadGraph[state.place])};
+}
+
+VillageState.random = function(parcelCount = 5) {
+  let parcels = [];
+  for (let i = 0; i < parcelCount; i++) {
+    let address = randomPick(Object.keys(roadGraph));
+    let place;
+    do {
+      place = randomPick(Object.keys(roadGraph));
+    } while (place == address);
+    parcels.push({place, address});
+  }
+  return new VillageState("Post Office", parcels);
+};
+
+const mailRoute = [
+  "Alice's House", "Cabin", "Alice's House", "Bob's House",
+  "Town Hall", "Daria's House", "Ernie's House",
+  "Grete's House", "Shop", "Grete's House", "Farm",
+  "Marketplace", "Post Office"
+];
+
+function routeRobot(state, memory) {
+  if (memory.length == 0) {
+    memory = mailRoute;
+  }
+  return {direction: memory[0], memory: memory.slice(1)};
+}
+
+function findRoute(graph, from, to) {
+  let work = [{at: from, route: []}];
+  for (let i = 0; i < work.length; i++) {
+    let {at, route} = work[i];
+    for (let place of graph[at]) {
+      if (place == to) return route.concat(place);
+      if (!work.some(w => w.at == place)) {
+        work.push({at: place, route: route.concat(place)});
+      }
+    }
+  }
+}
+
+function goalOrientedRobot({place, parcels}, route) {
+  if (route.length == 0) {
+    let parcel = parcels[0];
+    if (parcel.place != place) {
+      route = findRoute(roadGraph, place, parcel.place);
+    } else {
+      route = findRoute(roadGraph, place, parcel.address);
+    }
+  }
+  return {direction: route[0], memory: route.slice(1)};
+}
+
+// runRobot(VillageState.random(), goalOrientedRobot, []);
+
+// EXERCISES: MEASURING A ROBOT
+function getTurnCount(state, robot, memory) {
+  for (let turn = 0;; turn++) {
+    if (state.parcels.length == 0) {
+      return turn;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+  }
+}
+
+function compareRobots (robot1, memory1, robot2, memory2) {
+  let results1 = [];
+  let results2 = [];
+
+  for (let i = 0; i < 100; i++) {
+    let state = VillageState.random(Math.floor(Math.random()*100));
+    results1.push(getTurnCount(state, robot1, memory1));
+    results2.push(getTurnCount(state, robot2, memory2));
+  }
+
+  console.log((results1.reduce((acc, curr) => acc + curr) / 100).toPrecision(4));
+  console.log((results2.reduce((acc, curr) => acc + curr) / 100).toPrecision(4));
+}
+
+// compareRobots(randomRobot, [], goalOrientedRobot, []);
+// compareRobots(routeRobot, [], goalOrientedRobot, []);
+
+
+// EXERCISES: ROBOT EFFICIENCY
+
+function myRobot ({place, parcels}, route) {
+
+}
+
+// compareRobots(myRobot, [], goalOrientedRobot, []);
+
+
+// EXERCISES: PERSISTENT GROUP
+class PGroup {
+  constructor (values) {
+    this.values = values;
+  }
+
+  add (item) {
+    let newvalues = [];
+    for (let value of this.values) {
+      newvalues.push(value);
+    }
+    newvalues.push(item);
+    return new PGroup(newvalues);
+  }
+
+  delete (item) {
+    let newvalues = [];
+    for (let value of this.values) {
+      newvalues.push(value);
+    }
+    newvalues.splice(newvalues.indexOf(item), 1);
+    return new PGroup(newvalues);
+  }
+
+  has (item) {
+    return (this.values.indexOf(item) === -1) ? false : true;
+  }
+}
+PGroup.empty = new PGroup([]);
+
+// let a = PGroup.empty.add("a");
+// let ab = a.add("b");
+// let b = ab.delete("a");
+//
+// console.log(b.has("b"));
+// console.log(a.has("b"));
+// console.log(b.has("a"));
